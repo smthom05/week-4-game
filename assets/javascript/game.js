074441f1efdbbeed36e1d3858cc999bd6ba17ffe
Scott Thomas <scottthomas@Scotts-MacBook-Pro.local>
2017-12-16 18:57:39 -0700

    revamped HTML with bootstrap

---
+++ assets/javascript/game.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+//  GLOBAL VARIABLES
+// ====================================================
+
+
+
+
+ // GAME COUNTERS
+ var winCount = 0;
+ var lossCount = 0;
+ var userTotal = 0;
+ var targetTotal = 0;
+ var isTargetTotal = false;
+ var isOverTargetTotal = false;
+
+// FUNCTIONS
+// ====================================================
+
+$(".start-game").on("click", function(){
+
+  if
+
+  winCount = 0;
+  lossCount = 4;
+  userTotal = 0;
+  targetTotal = 0;
+  isTargetTotal = false;
+  isOverTargetTotal = false;
+
+};
+
+
+
+
+
+
+
+
+// MAIN PROCESS
+// =======================================================
+
+
+
+$(".start-game").on("click", function(){
+
+
+});
+
+
+
+
+
+
+
+
+
+
+});
