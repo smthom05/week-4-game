e85cc801bd43f3fdcaa4061388065e0a9475048e
Scott Thomas <scottthomas@Scotts-MacBook-Pro.local>
2017-12-17 12:33:37 -0700

    first working prototype

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,119 @@
 $(document).ready(function() {
 
-//  GLOBAL VARIABLES
-// ====================================================
+  //  GLOBAL VARIABLES
+  // ====================================================
+
+  // Generate our random target Score
+  var targetTotal = Math.floor((Math.random() * 102) + 19);
+
+  // Debugging / Checks
+  console.log(targetTotal);
+
+  // Assign our crystals random values
+  var redCrystal = Math.floor((Math.random()*12)+1);
+  var blueCrystal = Math.floor((Math.random()*12)+1);
+  var yellowCrystal = Math.floor((Math.random()*12)+1);
+  var greenCrystal = Math.floor((Math.random()*12)+1);
+
+  // Debugging / Checks
+  console.log(redCrystal);
+  console.log(blueCrystal);
+  console.log(yellowCrystal);
+  console.log(greenCrystal);
+
+  // GAME COUNTERS
+  var winCount = 0;
+  var lossCount = 0;
+  var userTotal = 0;
 
 
+  // FUNCTIONS
+  // ====================================================
+  function reset(){
+    targetTotal = Math.floor((Math.random() * 102) + 19);
+    userTotal = 0;
+    redCrystal = Math.floor((Math.random()*12)+1);
+    blueCrystal = Math.floor((Math.random()*12)+1);
+    yellowCrystal = Math.floor((Math.random()*12)+1);
+    greenCrystal = Math.floor((Math.random()*12)+1);
+    $("#player-score").text(userTotal);
+    $("#random-score").text(targetTotal);
 
+  };
 
- // GAME COUNTERS
- var winCount = 0;
- var lossCount = 0;
- var userTotal = 0;
- var targetTotal = 0;
- var isTargetTotal = false;
- var isOverTargetTotal = false;
+  // MAIN PROCESS
+  // =======================================================
 
-// FUNCTIONS
-// ====================================================
+  // Getting all of our global variable values into the HTML
+  $("#random-score").text(targetTotal);
+  $("#wins").text(winCount);
+  $("#losses").text(lossCount);
+  $("#player-score").text(userTotal);
 
-$(".start-game").on("click", function(){
+  // Attaching a data value to our crystal buttons
 
-  if
+  $("#red-crystal").on("click", function() {
 
-  winCount = 0;
-  lossCount = 4;
-  userTotal = 0;
-  targetTotal = 0;
-  isTargetTotal = false;
-  isOverTargetTotal = false;
+    userTotal += redCrystal;
+    $("#player-score").text(userTotal)
 
-};
+    if (userTotal === targetTotal) {
+      winCount ++;
+      $("#wins").text(winCount);
+      reset();
+    } else if (userTotal > targetTotal) {
+      lossCount ++;
+      $("#losses").text(lossCount);
+      reset();
+    }
+  });
 
+  $("#blue-crystal").on("click", function() {
 
+    userTotal += blueCrystal;
+    $("#player-score").text(userTotal)
 
+    if (userTotal === targetTotal) {
+      winCount ++;
+      $("#wins").text(winCount);
+      reset();
+    } else if (userTotal > targetTotal) {
+      lossCount ++;
+      $("#losses").text(lossCount);
+      reset();
+    }
+  });
 
+  $("#yellow-crystal").on("click", function() {
 
+    userTotal += yellowCrystal;
+    $("#player-score").text(userTotal)
 
+    if (userTotal === targetTotal) {
+      winCount ++;
+      $("#wins").text(winCount);
+      reset();
+    } else if (userTotal > targetTotal) {
+      lossCount ++;
+      $("#losses").text(lossCount);
+      reset();
+    }
+  });
 
+  $("#green-crystal").on("click", function() {
 
-// MAIN PROCESS
-// =======================================================
+    userTotal += greenCrystal;
+    $("#player-score").text(userTotal)
 
-
-
-$(".start-game").on("click", function(){
-
-
-});
-
-
-
-
-
-
-
-
-
+    if (userTotal === targetTotal) {
+      winCount ++;
+      $("#wins").text(winCount);
+      reset();
+    } else if (userTotal > targetTotal) {
+      lossCount ++;
+      $("#losses").text(lossCount);
+      reset();
+    }
+  });
 
 });
